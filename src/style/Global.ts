ecde1d348a8a21278a67acf80f4fcd276be9b277
chandrakant-karole <chandrakantkarole001995@gmail.com>
2022-02-14 16:50:45 +0530

    add trending on side menu

---
+++ src/style/Global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: radial-gradient(circle farthest-side,	#AFEEEE ,black);
+
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+  }
+`
+
+export default GlobalStyle
